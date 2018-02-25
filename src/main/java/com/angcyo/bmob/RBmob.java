770298be61dac703bbd5439f188376dae6693003
angcyo <angcyo@126.com>
2018-02-25 19:43:47 +0800

    fix base

--- src/main/java/com/angcyo/bmob/RBmob.java
+++ src/main/java/com/angcyo/bmob/RBmob.java
@@ -5,8 +5,19 @@ import android.content.pm.ApplicationInfo;
 import android.content.pm.PackageManager;
 import android.util.Log;
 
+import com.angcyo.uiview.net.P;
+import com.angcyo.uiview.net.RSubscriber;
+import com.angcyo.uiview.utils.RUtils;
+
+import java.util.List;
+
 import cn.bmob.v3.Bmob;
+import cn.bmob.v3.BmobObject;
 import cn.bmob.v3.BmobQuery;
+import cn.bmob.v3.exception.BmobException;
+import cn.bmob.v3.listener.FindListener;
+import cn.bmob.v3.listener.UpdateListener;
+import rx.Subscription;
 
 /**
  * Copyright (C) 2016,深圳市红鸟网络科技股份有限公司 All rights reserved.
@@ -59,5 +70,124 @@ public class RBmob {
     public static <T> BmobQuery<T> query() {
         BmobQuery<T> query = new BmobQuery<>();
         return query;
+    }
+
+    /**
+     * 保存新的数据, 保存成功返回数据 objectId, 如果失败, 返回空字符串
+     */
+    public static <T extends BmobObject> Subscription save(T data, final OnSingleResult<String> onResult) {
+        return data.saveObservable().subscribe(new RSubscriber<String>() {
+            @Override
+            public void onSucceed(String bean) {
+                super.onSucceed(bean);
+                onResult.onResult(bean); //ObjectId 字符串类型
+            }
+
+            @Override
+            public void onError(int code, String msg) {
+                //super.onError(code, msg);
+                onResult.onResult("");
+            }
+        });
+    }
+
+    /**
+     * 根据条件, 如果找到了已经存在的数据, 则更新数据, 否在添加数据
+     */
+    public static <T extends BmobObject> Subscription update(Class<T> cls, final T data, String where, final OnSingleResult<String> onResult) {
+        return query(cls, where, new OnResult<T>() {
+            @Override
+            public void onResult(List<T> resultList) {
+                if (RUtils.isListEmpty(resultList)) {
+                    save(data, onResult);
+                } else {
+                    final String objectId = resultList.get(0).getObjectId();
+                    data.update(objectId, new UpdateListener() {
+                        @Override
+                        public void done(BmobException e) {
+                            if (e == null) {
+                                onResult.onResult(objectId);
+                            } else {
+                                onResult.onResult("");
+                            }
+                        }
+                    });
+                }
+            }
+        });
+    }
+
+    /**
+     * 根据相等的条件, 查询数据
+     *
+     * @param where "key:value" 的格式
+     */
+    public static <T extends BmobObject> Subscription query(Class<T> cls, String where, final OnResult<T> onResult) {
+        final BmobQuery<T> query = new BmobQuery<>();
+        P.foreach(new P.OnValue() {
+            @Override
+            public void onValue(String key, String value) {
+                query.addWhereEqualTo(key, value);
+            }
+        }, where);
+
+        return query.findObjectsObservable(cls).subscribe(new RSubscriber<List<T>>() {
+            @Override
+            public void onSucceed(List<T> bean) {
+                super.onSucceed(bean);
+                onResult.onResult(bean);
+            }
+
+            @Override
+            public void onError(int code, String msg) {
+                //super.onError(code, msg);
+                onResult.onResult(null);
+            }
+        });
+    }
+
+    /**
+     * 删除满足查询条件的所有数据
+     */
+    public static <T extends BmobObject> Subscription delete(Class<T> cls, String where, final OnResult<T> onResult) {
+        return query(cls, where, new OnResult<T>() {
+            @Override
+            public void onResult(final List<T> resultList) {
+                if (RUtils.isListEmpty(resultList)) {
+                    //删除失败, 参数为null
+                    onResult.onResult(null);
+                } else {
+                    for (T item : resultList) {
+                        item.delete(new UpdateListener() {
+                            @Override
+                            public void done(BmobException e) {
+                                if (e == null) {
+                                    //删除成功, 返回被删除的数据
+                                    onResult.onResult(resultList);
+                                } else {
+                                    onResult.onResult(null);
+                                }
+                            }
+                        });
+                    }
+                }
+            }
+        });
+    }
+
+    interface OnResult<T> {
+        void onResult(List<T> resultList);
+    }
+
+    interface OnSingleResult<T> {
+        void onResult(T result);
+    }
+
+    public static class RFindListener<T> extends FindListener<T> {
+
+        @Override
+        public void done(List<T> resultList, BmobException exception) {
+
+        }
     }
 }
